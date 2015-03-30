8faea9e2e5ef7de01a8e53cead336258eac94cc2
Steve Robbins <steven.j.robbins@gmail.com>
2015-03-29 19:36:30 -0700

    init

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-Download: <a href="/mga.phar">mga.phar</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Magento Guest Audit</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Magento Guest Audit</h1>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            <h5>Download</h5>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><a href="/mga.phar">mga.phar</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
