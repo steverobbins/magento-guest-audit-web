d37f4d81b844d321153a9e7567a04f10d6cf7b6f
Steve Robbins <steven.j.robbins@gmail.com>
2015-04-04 16:34:44 -0700

    adding ga

--- index.php
+++ index.php
@@ -9,12 +9,24 @@
 <body>
     <div class="container">
         <div class="jumbotron">
-            <h1>Magento Guest Audit</h1>
+            <h1><?php echo file_get_contents('/var/www/html/mga.project.steverobbins.name/version') ?></h1>
         </div>
         <div class="page-header">
             <h3>Download</h3>
         </div>
         <a class="btn btn-lg btn-primary" href="/mga.phar">mga.phar</a>
+        <div class="page-header">
+            <h3>Source</h3>
+        </div>
+        <a class="btn btn-lg btn-primary" href="https://github.com/steverobbins/magento-guest-audit">GitHub</a>
     </div>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-16126282-21', 'auto');
+        ga('send', 'pageview');
+    </script>
 </body>
 </html>
--- make-phar.sh
+++ make-phar.sh
@@ -7,6 +7,7 @@ BIN_COMPOSER=/usr/local/bin/composer
 REPO_URL=https://github.com/steverobbins/magento-guest-audit.git
 REPO_FOLDER=$ROOT/mga
 PHAR_DEST=$ROOT/mga.phar
+VERSION=$ROOT/version
 
 if [ ! -f $REQUEST ]; then
   exit 0
@@ -15,8 +16,9 @@ fi
 rm -rf $REPO_FOLDER $REQUEST
 git clone $REPO_URL $REPO_FOLDER
 cd $REPO_FOLDER
-$BIN_COMPOSER install
+$BIN_COMPOSER --no-dev install
 $BIN_BOX build -c box.json
 rm -f $PHAR_DEST
 mv $REPO_FOLDER/mga.phar $PHAR_DEST
+php $PHAR_DEST --version >> $VERSION
 echo Done
